763ccc09bfb052b65e7158e0c0b92b56f0b2d9c2
Johannes Nynäs <johannes.nynas@gmail.com>
2013-02-22 14:29:23 +0200

    commands to post.py

--- scripts/post.py
+++ scripts/post.py
@@ -1,6 +1,46 @@
-import requests,json
+import requests,json, sys
 
-payload= { "_type" : "mytype" }
+commands = [
+#launch_url = 0
+ {
+    '_type': 'launch_url',
+    'client': "asha_311_rd",
+    'args' : {
+        'url' : 'http://google.com'
+    }
+},
+
+#find_files = 1
+{
+    '_type' : 'find_files',
+    'request_cookie': "decafbad",
+    'args' : {
+        'rootdir' : "c:/",
+
+    }
+},
+
+#download_files = 2
+ {
+    '_type': 'download_files',
+    'args': {
+        'files': {
+           'url': "http://download.pics.com/lolcat.png",
+            'targetdir': "c:/lolcat.png"
+        }
+    }
+
+},
+
+#upload_files = 3
+{
+    '_type': "upload_files",
+    'request_cookie': "decafbad",
+    'args': {
+        'files': ["c:/lolcat.png"]
+
+    }
+}]
 
 headers = {'Content-type': 'application/json' }
 
@@ -9,7 +49,7 @@ url = addr + "/messages/testchannel/?clientid=tester"
 
 print "POST",url
 
-r = requests.post(url, data = json.dumps(payload), headers = headers)
+r = requests.post(url, data = json.dumps(commands[int(sys.argv[1])]), headers = headers)
 
 print r.content
 
